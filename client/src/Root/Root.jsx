2f8b73aeae2900a53544c320dfee222152346c34
akbor shanto <akborshanto2002@gmail.com>
2024-08-14 23:45:14 -0700

    initital client  & server

---
+++ client/src/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../shared/Footer'
+
+const Root = () => {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <Outlet></Outlet>
+    <Footer></Footer>
+    
+    </div>
+  )
+}
+
+export default Root
